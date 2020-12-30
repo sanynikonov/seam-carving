77ceb152b82f4a35075822e78e041f2690cd901b
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-12-30 13:17:31 +0200

    Creates extensions for color matrix

---
+++ SeamCarving/ColorMatrixExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SeamCarving
+{
+    public static class ColorMatrixExtensions
+    {
+        public static int Height(this Color[,] colors)
+        {
+            return colors.GetLength(0);
+        }
+
+        public static int Width(this Color[,] colors)
+        {
+            return colors.GetLength(1);
+        }
+    }
+}
