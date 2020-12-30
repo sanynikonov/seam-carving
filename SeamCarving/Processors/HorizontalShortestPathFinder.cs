acaeadf5078fbb6fec9aed55cd4d583232354478
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-12-30 14:23:52 +0200

    Adds horizontal processors

---
+++ SeamCarving/Processors/HorizontalShortestPathFinder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SeamCarving.Processors
+{
+    public class HorizontalShortestPathFinder : BaseProcessor
+    {
+        public override void Process(ProcessingContext context)
+        {
+            var image = context.Result ?? context.Source;
+
+            var result = new List<Point>();
+
+            var energies = new int[image.Height()][];
+
+            for (int i = 0; i < energies.Length; i++)
+            {
+                energies[i] = new int[image.Width()];
+            }
+
+            for (int y = 0; y < image.Height(); y++)
+            {
+                for (int x = 0; x < image.Width(); x++)
+                {
+                    energies[y][x] = image[y, x].R; //RGB are equal, so no need to calculate average
+                }
+            }
+
+            var indexX = energies[0].Length - 1;
+            var minEnergyIndexY = energies.Select((arr) => arr[indexX]).Min();
+
+            result.Add(new Point(indexX, minEnergyIndexY));
+
+            while (--indexX >= 0)
+            {
+                var energiesLeftPoints = GetEnergiesLeft(energies, minEnergyIndexY, indexX + 1);
+
+                var minEnergyLeft = energiesLeftPoints.Select(p => energies[p.Y][p.X]).Min();
+
+                minEnergyIndexY = energiesLeftPoints.First(p => energies[p.Y][p.X] == minEnergyLeft).Y;
+
+                result.Add(new Point(indexX, minEnergyIndexY));
+            }
+
+            context.PointsOfShortestPath = result;
+            ProcessNextIfNotNull(context);
+        }
+
+        private Point[] GetEnergiesLeft(int[][] energies, int y, int x)
+        {
+            var possiblePoints = new[] { new Point(x - 1, y - 1), new Point(x - 1, y), new Point(x - 1, y + 1) };
+
+            return possiblePoints
+                .Where(p => p.IsInRange(energies.Length, energies[0].Length))
+                .ToArray();
+        }
+    }
+}
---
+++ SeamCarving/Processors/HorizontalShortestPathRemover.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SeamCarving.Processors
+{
+    public class HorizontalShortestPathRemover : BaseProcessor
+    {
+        public override void Process(ProcessingContext context)
+        {
+            if (context.PointsOfShortestPath == null)
+                throw new InvalidOperationException("Unable to remove shortest path without provided coodinates.");
+
+            var image = context.Source;
+
+            var result = new Color[image.Height() - 1, image.Width()];
+
+            int currentX = 0;
+            int currentY = 0;
+
+            for (int x = 0; x < image.Height(); x++)
+            {                
+                for (int y = 0; y < image.Width(); y++)
+                {
+                    if (context.PointsOfShortestPath.Contains(new Point(x, y)))
+                    {
+                        continue;
+                    }
+
+                    result[currentY++, currentX] = image[y, x];
+                }
+
+                currentX++;
+                currentY = 0;
+            }
+
+            context.Result = result;
+            ProcessNextIfNotNull(context);
+        }
+    }
+}
