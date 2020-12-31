a2e9f3e9daef2e53924f7af0d48ed8f064fbc8bc
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-12-31 18:53:43 +0200

    Creates generic processor

---
+++ SeamCarving/Processors/GenericProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeamCarving.Processors
+{
+    public class GenericProcessor : BaseProcessor
+    {
+        private Action<ProcessingContext> _processingAction;
+
+        public GenericProcessor(Action<ProcessingContext> processingAction)
+        {
+            _processingAction = processingAction;
+        }
+
+        public override void Process(ProcessingContext context)
+        {
+            _processingAction(context);
+            ProcessNextIfNotNull(context);
+        }
+    }
+}
