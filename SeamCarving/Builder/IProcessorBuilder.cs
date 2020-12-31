f21d580cb8fc31dfbb640a8451c16bf4b0494fde
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-12-31 18:54:01 +0200

    Creates process builder

---
+++ SeamCarving/Builder/IProcessorBuilder.cs
@@ -0,0 +1,15 @@
+﻿using SeamCarving.Processors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeamCarving.Builder
+{
+    public interface IProcessorBuilder
+    {
+        IProcessorBuilder SetProcessor(IProcessor processor);
+
+        IProcessorBuilder Reset();
+        IProcessor Build();
+    }
+}
---
+++ SeamCarving/Builder/ProcessorBuilder.cs
@@ -0,0 +1,40 @@
+﻿using SeamCarving.Processors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeamCarving.Builder
+{
+    public class ProcessorBuilder : IProcessorBuilder
+    {
+        private IProcessor _firstProcessor;
+        private IProcessor _lastProcessor;
+
+        public IProcessor Build()
+        {
+            return _lastProcessor;
+        }
+
+        public IProcessorBuilder Reset()
+        {
+            _firstProcessor = null;
+            return this;
+        }
+
+        public IProcessorBuilder SetProcessor(IProcessor processor)
+        {
+            if (_firstProcessor == null)
+            {
+                _firstProcessor = processor;
+                _lastProcessor = _firstProcessor;
+            }
+            else
+            {
+                _lastProcessor.SetNext(processor);
+                _lastProcessor = processor;
+            }
+
+            return this;
+        }
+    }
+}
