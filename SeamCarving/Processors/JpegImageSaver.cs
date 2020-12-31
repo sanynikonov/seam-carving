ccff39b9aef032ff60950d874c2dbc3071222668
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-12-31 19:09:08 +0200

    Creates processor for saving images

---
+++ SeamCarving/Processors/JpegImageSaver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace SeamCarving.Processors
+{
+    public class JpegImageSaver : BaseProcessor
+    {
+        private readonly string _destination;
+        private readonly int _width;
+        private readonly int _height;
+
+        public JpegImageSaver(string destination, int width, int height)
+        {
+            _destination = destination;
+            _width = width;
+            _height = height;
+        }
+
+        public override void Process(ProcessingContext context)
+        {
+            using var resultBitmap = BitmapConverter.FromColorsMatrix(context.Result);
+            using var finalBitmap = new Bitmap(resultBitmap, new Size(_width, _height));
+
+            finalBitmap.Save(_destination, ImageFormat.Jpeg);
+        }
+    }
+}
